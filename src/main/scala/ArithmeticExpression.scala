40cd51d2c4b67946de3521da7b2998d4f2e5b685
Olja <simonnomis2002@gmail.com>
2023-10-28 17:54:15 +0200

    ScalaTest

--- src/main/scala/ArithmeticExpression.scala
+++ src/main/scala/ArithmeticExpression.scala
@@ -1,3 +1,14 @@
-class ArithmeticExpression {
+enum ArithmeticExpression:
+  case Num(value: Int)
+  case Minus(expression: ArithmeticExpression)
+  case Plus(left: ArithmeticExpression, right: ArithmeticExpression)
+  case Mult(left: ArithmeticExpression, right: ArithmeticExpression)
+  case Div(left: ArithmeticExpression, right: ArithmeticExpression)
 
-}
+/* type Num = Int
+  type Minus = ArithmeticExpression
+  type Plus = ArithmeticExpression
+  type Mult = ArithmeticExpression
+  type Div = ArithmeticExpression
+ */
+
