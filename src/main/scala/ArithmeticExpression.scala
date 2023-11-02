06b5ca1e340d792e17fa20a2eaa430cdaf6f24b1
Olja <simonnomis2002@gmail.com>
2023-11-02 17:21:53 +0100

    lists:

--- src/main/scala/ArithmeticExpression.scala
+++ src/main/scala/ArithmeticExpression.scala
@@ -1,3 +1,4 @@
+
 enum ArithmeticExpression:
   case Num(value: Int)
   case Minus(value: ArithmeticExpression)
@@ -7,10 +8,15 @@ enum ArithmeticExpression:
 
   case Pow(base: ArithmeticExpression, exponent: ArithmeticExpression)
 object ArithmeticExpression {
-  /*@main def hello(): Unit = {
-    println(evaluate(Pow(Num(2), Minus(Num(2)))))
+  @main def hello(): Unit = {
+    //println(evaluate(Pow(Num(2), Minus(Num(2)))))
+    val listOfExp = List(Num(6), Minus(Num(89)), Plus(Num(10), Num(2)), Pow(Num(5), Minus(Num(1))))
+    val listOfDouble = evaluate(listOfExp)
+    println(listOfDouble)
+    val string: String = showResults(listOfExp)
+    println(string)
   }
-  */
+
   def evaluate(expression: ArithmeticExpression): Double = expression match
     case Num(value: Int) => value.toDouble
     case Minus(value: ArithmeticExpression) => -evaluate(value)
@@ -19,31 +25,53 @@ object ArithmeticExpression {
     case Div(l: ArithmeticExpression, r: ArithmeticExpression) => evaluate(l) / evaluate(r)
 
     case Pow(base: ArithmeticExpression, exponent: ArithmeticExpression) => {
-      if ((evaluate(exponent) == 0) && (evaluate(base) != 0))  1.0
-      else if (evaluate(exponent) > 0)  evaluate(base) * evaluate(Pow(Num(evaluate(base).toInt), Num(evaluate(exponent).toInt - 1)))
+      if ((evaluate(exponent) == 0) && (evaluate(base) != 0)) 1.0
+      else if (evaluate(exponent) > 0) evaluate(base) * evaluate(Pow(Num(evaluate(base).toInt), Num(evaluate(exponent).toInt - 1)))
       else (1 / evaluate(base)) * evaluate(Pow(Num(evaluate(base).toInt), Num(evaluate(exponent).toInt + 1)))
     }
-    /*case Pow(base: ArithmeticExpression, exponent: ArithmeticExpression) => (base, exponent) match{
-      case (_, Num(0)) if evaluate(base).toInt != 0 => 1.0
-      case (Num(0), _) if evaluate(exponent).toInt != 0 => 0.0
-      case _ if evaluate(exponent).toInt > 0 => evaluate(base) * Pow(base, exponent-1)
-    }
-    */
 
   def pretty(expression: ArithmeticExpression): String =
     expression match
-    case Num(value: Int) => s"${evaluate(Num(value))}"
-    case Minus(value) =>
-      s"-${pretty(value)}"
-    case Plus(l, r) =>
-      s"${pretty(l)} + ${pretty(r)}"
-    case Mult(l, r) =>
-      s"${pretty(l)} * ${pretty(r)}"
-    case Div(l, r) =>
-      s"${pretty(l)} / ${pretty(r)}"
-    case Pow(b, e) =>
-      s"${pretty(b)} ^ ${pretty(e)}"
+      case Num(value: Int) => s"${evaluate(Num(value))}"
+      case Minus(value) =>
+        s"-${pretty(value)}"
+      case Plus(l, r) =>
+        s"${pretty(l)} + ${pretty(r)}"
+      case Mult(l, r) =>
+        s"${pretty(l)} * ${pretty(r)}"
+      case Div(l, r) =>
+        s"${pretty(l)} / ${pretty(r)}"
+      case Pow(b, e) =>
+        s"${pretty(b)} ^ ${pretty(e)}"
 
+
+  def evaluate(expressions: List[ArithmeticExpression]): List[Double] =
+    expressions match {
+      case Nil => Nil
+      case head :: tail =>
+        val kopf = head match {
+          case Num(value) => value.toDouble
+          case Minus(exp) => -evaluate(List(exp)).head
+          case Plus(exp1, exp2) =>
+            evaluate(List(exp1)).head + evaluate(List(exp2)).head
+          case Mult(exp1, exp2) =>
+            evaluate(List(exp1)).head * evaluate(List(exp2)).head
+          case Div(exp1, exp2) =>
+            evaluate(List(exp1)).head / evaluate(List(exp2)).head
+          case Pow(basis, exp) =>
+            if (evaluate(List(exp)).head == 0 && evaluate(List(basis)).head != 0) 1.0
+            else if (evaluate(List(exp)).head > 0)
+              evaluate(List(basis)).head * evaluate(List(Pow(basis, Num(evaluate(List(exp)).head.toInt - 1)))).head
+            else
+              1 / evaluate(List(basis)).head * evaluate(List(Pow(basis, Num(evaluate(List(exp)).head.toInt + 1)))).head
+        }
+        kopf :: evaluate(tail)
+    }
+  def showResults(expressions: List[ArithmeticExpression]): String =
+    expressions match{
+      case Nil => ""
+      case head :: tail => "\n" + pretty(expressions.head) + s" = ${evaluate(expressions.head)}" + showResults(expressions.tail)
+  }
 }
 
 
--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -1,12 +1,13 @@
+import ArithmeticExpression.{Div, Minus, Mult, Num, Plus, Pow, showResults}
 import org.scalatest.funsuite.AnyFunSuite
 class test extends AnyFunSuite {
-  test("Num is double and has valid value"){
+  test("Num is double and has the valid value"){
     assert(ArithmeticExpression.evaluate(ArithmeticExpression.Num(7)) === 7.0)
   }
-  test("Minus is double and has valid negative value"){
+  test("Minus is double and has the valid negative value"){
     assert(ArithmeticExpression.evaluate(ArithmeticExpression.Minus(ArithmeticExpression.Num(4))) === -4.0)
   }
-  test("Pow is double and has valid value"){
+  test("Pow is double and has the valid value"){
     assert(ArithmeticExpression.evaluate(ArithmeticExpression.Pow(ArithmeticExpression.Num(2), ArithmeticExpression.Num(6))) === 64.0)
   }
   test("Pretty for Mult is pretty"){
@@ -17,5 +18,11 @@ class test extends AnyFunSuite {
   }
   test("Pretty for Pow is pretty"){
     assert(ArithmeticExpression.pretty(ArithmeticExpression.Pow(ArithmeticExpression.Num(2), ArithmeticExpression.Num(6))) === "2.0 ^ 6.0")
+  }
+  test("evaluate for List of expressions functions correctly"){
+    assert(ArithmeticExpression.evaluate(List(Minus(Num(89)), Plus(Num(10), Num(2)), Pow(Num(5), Minus(Num(1))))) === List(-89.0, 12.0, 0.2))
+  }
+  test("showResults functions correctly"){
+    assert(showResults(List(Num(6), Minus(Num(89)), Plus(Num(10), Num(2)), Pow(Num(5), Minus(Num(1))))) === "\n6.0 = 6.0\n-89.0 = -89.0\n10.0 + 2.0 = 12.0\n5.0 ^ -1.0 = 0.2")
   }
 }
